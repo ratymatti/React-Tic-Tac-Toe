f8a78b5367cdbdad3f1bdaacd50e38332a7e42ee
Matti Räty <mattiraty@hotmail.com>
2024-04-15 16:02:45 +0300

    created gameState -variable that holds all game related state

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { useState } from "react"
 import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
 import Log from "./components/Log"
-import getPoints from "./functions/getPoints"
+import  { PointCount, functions } from "./functions/gameLogic"
+import GameOver from "./components/GameOver"
 
 export interface GameTurn {
     player: PlayerSymbol;
@@ -15,47 +16,60 @@ export interface GameTurn {
 
 export enum PlayerSymbol {
     X = 'X',
-    O = 'O'
+    O = 'O',
+    TIE = 'TIE'
 }
 
-function deriveCurrentPlayer(turns: GameTurn[]): PlayerSymbol {
-    let currentPlayer = PlayerSymbol.X;
+export type GameBoardType = (PlayerSymbol | null)[][];
 
-    if (turns.length > 0 && turns[0].player === PlayerSymbol.X) {
-        currentPlayer = PlayerSymbol.O;
-    }
-    return currentPlayer;
+export const initialGameBoard: GameBoardType = [
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null]
+];
+
+const initialGameState = {
+    gameTurns: [],
+    winner: null,
+    gameBoard: JSON.parse(JSON.stringify(initialGameBoard)),
+    currentPlayer: PlayerSymbol.X,
+    gamePoints: { xPoints: 0, oPoints: 0 }
+};
+
+export interface GameState {
+    gameTurns: GameTurn[];
+    winner: PlayerSymbol | null;
+    gameBoard: GameBoardType;
+    currentPlayer: PlayerSymbol;
+    gamePoints: PointCount;
 }
 
 function App() {
-    const [gameTurns, setGameTurns] = useState<GameTurn[]>([]);
-
-    const currentPlayer = deriveCurrentPlayer(gameTurns);
+    const [gameState, setGameState] = useState<GameState>(JSON.parse(JSON.stringify(initialGameState)));
 
     function handleSelectSquare(rowIndex: number, colIndex: number) {
-        setGameTurns((prevTurns) => {
-            const currentPlayer = deriveCurrentPlayer(prevTurns);
-
-            const updatedTurns = [
-                { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
-                ...prevTurns
-            ];
-            return updatedTurns;
+        setGameState((prevState) => {
+            const newCurrentPlayer = prevState.currentPlayer === PlayerSymbol.X ? PlayerSymbol.O : PlayerSymbol.X;
+            const updatedTurns = functions.updateTurns(prevState, rowIndex, colIndex);
+            const newBoard = functions.updateBoard(prevState, rowIndex, colIndex);
+            const updatedPoints = functions.updatePoints(prevState, updatedTurns);
+            const winner = functions.determineWinner(updatedPoints, updatedTurns);
+    
+            return {
+                ...prevState,
+                currentPlayer: newCurrentPlayer,
+                gameTurns: updatedTurns,
+                gameBoard: newBoard,
+                gamePoints: updatedPoints,
+                winner: winner
+            };
         });
     }
 
-    useEffect(() => {
-        if (gameTurns.length < 5) {
-            return;
-        }
-        const { xPoints, oPoints } = getPoints(gameTurns);
-
-        if (xPoints === 3) {
-            console.log('Player 1 wins!');
-        } else if (oPoints === 3) {
-            console.log('Player 2 wins!');
-        }
-    }, [gameTurns]);
+    function handleRestart() {
+        setGameState(JSON.parse(JSON.stringify(initialGameState)));
+    }
 
     return (
         <main>
@@ -64,18 +78,21 @@ function App() {
                     <Player
                         initialName="Player 1"
                         symbol="X"
-                        isActive={currentPlayer === PlayerSymbol.X} />
+                        isActive={gameState.currentPlayer === PlayerSymbol.X} />
                     <Player
                         initialName="Player 2"
                         symbol="O"
-                        isActive={currentPlayer === PlayerSymbol.O} />
+                        isActive={gameState.currentPlayer === PlayerSymbol.O} />
                 </ol>
+                {gameState.winner && <GameOver
+                    winner={gameState.winner}
+                    handleRestart={handleRestart} />}
                 <GameBoard
                     onSelectSquare={handleSelectSquare}
-                    turns={gameTurns} />
+                    board={gameState.gameBoard} />
             </div>
             <Log
-                turns={gameTurns} />
+                turns={gameState.gameTurns} />
         </main>
     )
 }
