81bc69f69f3655a5af69e24669b983bf2e3d02af
Matti Räty <mattiraty@hotmail.com>
2024-04-15 12:25:28 +0300

    removed unnecessary state and changed how gameboard is initialized

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,21 +1,43 @@
 import React from "react";
+import { GameTurn, PlayerSymbol } from "../App";
 
-const initialGameBoard = [
+interface GameBoardProps {
+    turns: GameTurn[];
+    onSelectSquare: (rowIndex: number, colIndex: number) => void;
+}
+
+export type GameBoardType = (PlayerSymbol | null)[][];
+
+export const initialGameBoard: GameBoardType = [
     [null, null, null, null],
     [null, null, null, null],
     [null, null, null, null],
     [null, null, null, null]
 ];
 
-export default function GameBoard(): JSX.Element {
+export default function GameBoard({ onSelectSquare, turns }:GameBoardProps): JSX.Element {
+    let gameBoard: GameBoardType = initialGameBoard;
+
+    for (const turn of turns) {
+        const { player, square } = turn;
+        const { row, col } = square;
+
+        gameBoard[row][col] = player;
+    }
+
+
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => (
+            {gameBoard.map((row, rowIndex) => (
                 <li key={rowIndex} >
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex} className="cell">
-                                <button>{playerSymbol}</button>
+                                <button
+                                    onClick={() => onSelectSquare(rowIndex, colIndex)}
+                                    disabled={playerSymbol !== null}>
+                                        {playerSymbol}
+                                </button>
                             </li>
                         ))}
                     </ol>
