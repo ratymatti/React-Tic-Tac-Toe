f80a0a5ceb2924c0a14874ffb6319e82057e4b2b
Matti Räty <mattiraty@hotmail.com>
2024-04-15 16:05:06 +0300

    created GameOver component

---
+++ src/components/GameOver.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { PlayerSymbol } from "../App";
+
+interface GameOverProps {
+    winner: PlayerSymbol;
+    handleRestart: () => void;
+}
+
+export default function GameOver({ winner, handleRestart }: GameOverProps) {
+    return (
+        <div id="game-over">
+            <h2>Game Over!</h2>
+            <p>{winner} won!</p>
+            <p>
+                <button onClick={handleRestart}>Rematch!</button>
+            </p>    
+        </div>
+    )
+}
